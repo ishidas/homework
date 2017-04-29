685b1cea8ff094de2216540e6eacc87d93b5daca
sawakoishida <sawako.ishida@tangocard.com>
2017-04-29 11:37:28 -0700

    added edits

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -7,7 +7,11 @@ var addButton = document.querySelector(".add");
 var submitButton = document.querySelector("[type=submit]");
 
 var HouseholdObj = new Household();
+var returnedNode;
 var BrandNewPerson = null;
+// createEditButton();
+var editButton;
+var index = 0;
 
 //Household constructor
 function Household (value) {
@@ -22,12 +26,42 @@ Household.prototype.addPersonToHousehold = function (personObj) {
 }
 
 // Person Constructor
-function Person (next, prev) {
+function Person (index, next, prev) {
+  this.exist = false;
+  this.index = index;
   this.age = null;
   this.relationship = null;
   this.smoker = null;
   this.next = null;
   this.prev = null;
+  this.editButton = null;
+  this.submitEditButton = null;
+  this.editMethod =  function (){
+    console.log(`hitting ${this.age}`);
+    console.dir(`hitting ${this}`);
+    if(this.exist) {
+      return;
+    } else {
+      this.createFields();
+      this.exist = true;
+    }
+  }
+  this.submitEdit = function (){
+    console.log('this' + this.age);
+    returnedNode = HouseholdObj.search(this);
+    var age = document.querySelector(`.age-${this.index}`);
+    var rel = document.querySelector(`.rel-${this.index}`);
+    var smkr = document.querySelector(`.smoker-${this.index}`);
+    console.log(returnedNode);
+    console.log(`hittingSubmitEdit ${age.value}`);
+    console.log(`hittingSubmitEdit ${rel.value}`);
+    console.log(`hittingSubmitEdit ${smkr.checked}`);
+    console.log(returnedNode.age = parseInt(age.value));
+    console.log(returnedNode.relationship = rel.value);
+    console.log(returnedNode.smoker = smkr.checked);
+
+    console.log(HouseholdObj);
+  }
 }
 
 Person.prototype.addAge = function (age) {
@@ -44,6 +78,21 @@ Person.prototype.smokerTrueFalse = function (answer) {
   this.smoker = answer;
 }
 
+Person.prototype.incrementIndex = function () {
+  this.index = index;
+  index ++;
+}
+Household.prototype.search = function (searchNode) {
+
+  console.log('hitting saerch: ' + searchNode.index);
+  console.dir('head' + this.head.age);
+  var currentNode = this.head;
+  while(currentNode){
+    if(currentNode.index === searchNode.index) return currentNode;
+    currentNode = this.next;
+  }
+}
+
 //User friendly feature :D
 function clearField (ageField, relField, checkedSmoker) {
   ageField.value = '';
@@ -51,12 +100,11 @@ function clearField (ageField, relField, checkedSmoker) {
   checkedSmoker.checked = false;
 }
 
-function createPerson (event) {
+function createPerson () {
   var age = ageEl.value;
   var selectedRelElText = relEl.options[relEl.selectedIndex].text;
   var checkedSmoker = smokerEl.checked;
-  var BrandNewPerson = new Person(null, null);
-
+  var BrandNewPerson = new Person(index, null, null);
   if(!age || selectedRelElText === '---') {
     alert('age and relationship fields cannot be empty!');
     return
@@ -64,11 +112,67 @@ function createPerson (event) {
   BrandNewPerson.addAge(age);
   BrandNewPerson.addRelationship(selectedRelElText);
   BrandNewPerson.smokerTrueFalse(checkedSmoker);
-  HouseholdObj.addPersonToHousehold(BrandNewPerson);
+  BrandNewPerson.incrementIndex();
   addToTheListHTML(BrandNewPerson);
+  BrandNewPerson.createEditButton();
+  BrandNewPerson.createsubmitEditButton();
+  HouseholdObj.addPersonToHousehold(BrandNewPerson);
+
   clearField(ageEl, relEl, smokerEl);
   console.log(HouseholdObj);
   return;
+}
+
+function edit (PersonObj) {
+
+}
+
+Person.prototype.createsubmitEditButton = function () {
+  var self = this;
+  var submitButtonTag = document.createElement("button");
+  var submitButtonText = document.createTextNode("submit edit");
+  submitButtonTag.setAttribute("class",`submit-edit-button-${self.index}`);
+  var button = document.querySelector(".household").appendChild(submitButtonTag);
+  button.appendChild(submitButtonText);
+  self.submitEditButton = document.querySelector(`.submit-edit-button-${self.index}`);
+
+  self.submitEditButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    self.submitEdit();
+  });
+}
+Person.prototype.createEditButton = function () {
+  var self = this;
+  var buttonTag = document.createElement("button");
+  var buttonText = document.createTextNode("edit");
+  buttonTag.setAttribute("class",`edit-button-${self.index}`);
+  var button = document.querySelector(".household").appendChild(buttonTag);
+  button.appendChild(buttonText);
+  self.editButton = document.querySelector(`.edit-button-${self.index}`);
+
+  self.editButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    self.editMethod();
+  });
+}
+
+Person.prototype.createFields = function () {
+  var inputEl1 = document.createElement('input');
+  var clonedRelEl = relEl.cloneNode(true);
+  var clonedSmokerEl = smokerEl.cloneNode(true);
+  var relTag = document.createElement("label");
+  var relTagText = document.createTextNode("Relationship:");
+  var smokerTag = document.createElement("label");
+  var smokerTagText = document.createTextNode("Smoker?:");
+  inputEl1.setAttribute('class', `age-${this.index}`);
+  inputEl1.setAttribute("placeholder", "modify age here");
+  clonedRelEl.setAttribute('class', `rel-${this.index}`);
+  clonedSmokerEl.setAttribute('class', `smoker-${this.index}`);
+  document.querySelector('.household').insertBefore(inputEl1,document.querySelector(`.edit-button-${this.index}`));
+  document.querySelector('.household').insertBefore(relTag.appendChild(relTagText),document.querySelector(`.edit-button-${this.index}`));
+  document.querySelector('.household').insertBefore(clonedRelEl,document.querySelector(`.edit-button-${this.index}`));
+  document.querySelector('.household').insertBefore(smokerTag.appendChild(smokerTagText),document.querySelector(`.edit-button-${this.index}`));
+  document.querySelector('.household').insertBefore(clonedSmokerEl,document.querySelector(`.edit-button-${this.index}`));
 }
 
 function addToTheListHTML (personObj) {
@@ -76,24 +180,20 @@ function addToTheListHTML (personObj) {
   var text = document.createTextNode(`Age: ${personObj.age}, Relationship: ${personObj.relationship}, Smoker: ${personObj.smoker}`);
   li.appendChild(text);
   document.querySelector(".household").appendChild(li);
+  // personObj.createEditButton();
+
 }
-//
-function submitAndStoreData (event) {
-  // localStorage.setItem('', HouseholdArray);
-  ajax
-  clearField(ageEl, relEl, smokerEl);
-}
+
 
 
 //listeners
+
 addButton.addEventListener('click', function(event){
   event.preventDefault();
-  createPerson(event);
+  createPerson();
 });
 
 submitButton.addEventListener('click', function(event){
   event.preventDefault();
   submitAndStoreData(event);
-})
-// var hh = new Household(new Person(11, 'kid', false));
-// console.log(hh);
+});
