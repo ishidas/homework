f705b3141b66a558d14b11b537d3ae63a94d4882
sawakoishida <sawako.ishida@tangocard.com>
2017-06-09 00:05:10 -0700

    add and delete fine

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -6,14 +6,13 @@ var smokerEl = document.querySelector("[name=smoker]");
 var addButton = document.querySelector(".add");
 var submitButton = document.querySelector("[type=submit]");
 var HouseholdObj = new Household();
-var returnedNode;
-var BrandNewPerson;
-var editButton;
 var index = 0;
+var saveHousehold ,returnedNode,BrandNewPerson, editButton,currentDate;
 
-//Household constructor
+
+/*** Household Constructor***/
 function Household (value) {
-  this.len = 0;
+  this._len = 0;
   this.head = null;
 }
 
@@ -22,27 +21,74 @@ Household.prototype.addPersonToHousehold = function (newPerson) {
   currentNode = this.head;
   if(!this.head) {
     this.head = BrandNewPerson;
-    this.len++
+    this._len++
     return BrandNewPerson;
   }
   while(currentNode.next){
     currentNode = currentNode.next;
   }
   currentNode.next = BrandNewPerson;
-  this.len++
+  this._len++
   return BrandNewPerson;
 }
 
-// Person Constructor
+Household.prototype.remove = function (pos) {
+  var pos = parseInt(pos);
+  var currentNode = this.head, counter = 1, prevNode;
+  //if no node, return
+  if(this._len === 0) return;
+  //when only one node
+  if(pos === 1 && this._len === 1) {
+    this.head = currentNode.next;
+    this._len --;
+    counter = 1;
+    index --;
+  } else {
+    while(counter < pos){
+      prevNode = currentNode;
+      if (currentNode.next){
+        currentNode = currentNode.next;
+      } else {
+        currentNode = null;
+      }
+      counter++;
+    }
+    if(currentNode.next !== null) {
+      prevNode.next = currentNode.next;
+      prevNode.next.itemNum --;
+    } else {
+      prevNode.next = null;
+    }
+    index --;
+    this._len--;
+    // console.log(HouseholdObj);
+    return this.head;
+  }
+}
+
+Household.prototype.seachNodeAt = function (pos){
+  var currentNode = this.head, len = this._len, counter = 1;
+  if(len === 0 || pos < 1 || pos > len){
+    alert('There is no data in');
+    return;
+  }
+  while(counter < pos){
+    currentNode = currentNode.next;
+    counter++;
+  }
+  return currentNode;
+}
+
+/*** Person Constructor***/
 function Person () {
-  this.index = index;
+  this.itemNum = index;
   this.age = null;
   this.relationship = null;
   this.smoker = null;
   this.next = null;
-  this.deleteButton = null;
+  // this.deleteButton = null;
   this.submitEditButton = null;
-  this.removeElement = removeElement;
+  // this.removeElement = removeElement;
 }
 
 Person.prototype.addAge = function (age) {
@@ -59,42 +105,57 @@ Person.prototype.smokerTrueFalse = function (answer) {
   this.smoker = answer;
 }
 
-Person.prototype.addIndex = function () {
-  index++;
+Person.prototype.addItemNum = function () {
+  index ++;
+  this.itemNum ++;
 }
-Household.prototype.remove = function (pos) {
-  console.log('pos? ' + pos);
-   var currentNode = this.head;
-   var counter = 0;
-   var prevNode = null;
-   var nodeToDelete = null
-   var deletedNode = null;
-   if(pos === 0) {
-     this.head = null;
-     deletedNode = currentNode;
-     currentNode = null;
-     this.len --;
-     index --;
-     console.log(HouseholdObj);
-     return currentNode;
-   }
+// Person.prototype.deleteEditButton = function () {
+//   var self = this;
+//   var buttonTag = document.createElement("button");
+//   var buttonText = document.createTextNode("delete");
+//   // console.log();
+//   buttonTag.setAttribute("class","delete-button");
+//   var button = document.querySelector(".household").appendChild(buttonTag);
+//   button.appendChild(buttonText);
+//   self.deleteButton = document.querySelector(`.delete-button`);
+//   console.log(self.deleteButton);
+//   self.deleteButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log('hitting');
+//     self.removeElement();
+//     HouseholdObj.remove(self.index);
+//   });
+// }
 
-  while (counter < pos) {
-    prevNode = currentNode;
-    nodeToDelete = currentNode.next;
-    counter++
-  }
-  prevNode.next = nodeToDelete.next;
-  deletedNode = nodeToDelete;
-  nodeToDelete = null;
-  this.len--;
-  index --;
-  console.log(HouseholdObj);
-  return deletedNode;
+ Person.prototype.addToTheListHTML = function () {
+   var li = document.createElement("li");
+   var text = document.createTextNode(`Age: ${this.age}, Relationship: ${this.relationship}, Smoker: ${this.smoker}`);
+   var buttonTag = document.createElement("button");
+   var buttonText = document.createTextNode("delete");
+  li.setAttribute("data", `${this.itemNum}`);
+  buttonTag.setAttribute("class","delete-button");
+  buttonTag.appendChild(buttonText);
+  li.appendChild(text);
+  li.appendChild(buttonTag);
+  document.querySelector(".household").appendChild(li);
+
+  li.onclick = function (event){
+    HouseholdObj.remove(event.target.parentNode.getAttribute('data'));
+    this.parentNode.removeChild(this);
+    console.log(HouseholdObj);
+  };
 }
 
+// Person.prototype.removeElement = function (Obj) {
+//   var olEl = document.querySelector("ol");
+//   // var liEl = document.querySelector(`.li-${index}`);
+//   // var deleteButton = document.querySelector(`.delete-button-${index}`)
+//   // olEl.removeChild(deleteButton);
+//   // olEl.removeChild(liEl);
+//   olEl.removeChild(this.deleteButton);
+// }
 
-//User friendly feature :D
+
 function clearField (ageField, relField, checkedSmoker) {
   ageField.value = '';
   relField.options[relField.selectedIndex].selected = '---';
@@ -105,17 +166,19 @@ function createPerson () {
   var age = ageEl.value;
   var selectedRelElText = relEl.options[relEl.selectedIndex].text;
   var checkedSmoker = smokerEl.checked;
-  var BrandNewPerson = new Person(index, null, null);
+  var itemInLS = localStorage.getItem(currentDate);
+
+  BrandNewPerson = new Person();
   if(!age || selectedRelElText === '---') {
     alert('age and relationship fields cannot be empty!');
     return
   }
+  BrandNewPerson.addItemNum();
   BrandNewPerson.addAge(age);
   BrandNewPerson.addRelationship(selectedRelElText);
   BrandNewPerson.smokerTrueFalse(checkedSmoker);
-  BrandNewPerson.addIndex();
-  addToTheListHTML(BrandNewPerson);
-  BrandNewPerson.deleteEditButton();
+  BrandNewPerson.addToTheListHTML();
+
   HouseholdObj.addPersonToHousehold(BrandNewPerson);
 
   clearField(ageEl, relEl, smokerEl);
@@ -123,67 +186,6 @@ function createPerson () {
   return;
 }
 
-Person.prototype.deleteEditButton = function () {
-  var self = this;
-  var buttonTag = document.createElement("button");
-  var buttonText = document.createTextNode("delete");
-  buttonTag.setAttribute("class",`delete-button-${self.index}`);
-  var button = document.querySelector(".household").appendChild(buttonTag);
-  button.appendChild(buttonText);
-  self.deleteButton = document.querySelector(`.delete-button-${self.index}`);
-
-  self.deleteButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log('self.index' + self.index);
-    self.removeElement(self.index);
-    HouseholdObj.remove(self.index);
-  });
-}
-
-
-function addToTheListHTML (personObj) {
-  var li = document.createElement("li");
-  var text = document.createTextNode(`Age: ${personObj.age}, Relationship: ${personObj.relationship}, Smoker: ${personObj.smoker}`);
-  li.setAttribute("class", `li-${personObj.index}`);
-  li.appendChild(text);
-  document.querySelector(".household").appendChild(li);
-}
-
-function removeElement (index) {
-  var olEl = document.querySelector("ol");
-  var liEl = document.querySelector(`.li-${index}`);
-  var deleteButton = document.querySelector(`.delete-button-${index}`)
-  olEl.removeChild(deleteButton);
-  olEl.removeChild(liEl);
-}
-
-function submit (houseObj) {
-  var current = Date.now();
-  if(typeof(Storage) !== "undefined") {
-    localStorage.setItem(`household-${current}`, JSON.stringify(houseObj));
-    var data = localStorage.getItem(`household-${current}`);
-    formatJSON(data);
-    var preEl = document.querySelector(".debug");
-    var textNode = document.createTextNode(data);
-
-    preEl.appendChild(textNode);
-    preEl.style.display = "block";
-    alert('data stored!');
-  } else {
-    alert('There is not web storage support in your browser');
-  }
-}
-
-function formatJSON (jsonObj) {
-  var obj = JSON.parse(jsonObj);
-
-  for (var index in obj.head) {
-    obj.head[index] = 
-      // console.log(obj.head[index] + "\n");
-  }
-
-
-}
 //listeners
 addButton.addEventListener('click', function(event){
   event.preventDefault();
